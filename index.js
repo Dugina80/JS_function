0e7e6dcc9ba938209a6f3c8981f75452131ab39e
Dugina80 <alenka251280d@gmail.com>
2023-05-09 16:13:57 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+/*Напиши всі можливі варіанти створення функцій
+Function Declaration
+function showMessage () {
+    console.log (`Hello`);
+};
+showMessage ();
+Function Expression
+const showMessage = () => {
+     console.log (`Hello`);
+};
+showMessage ();
+
+
+Напиши функцію, яка обчислює факторіал переданого їй числа.
+  function factorial(n){
+    let result = 1;
+    while(n){
+        result *= n--;
+    }
+    return result;
+};
+alert( factorial(4) ); 
+
+Напиши функцію, яка приймає довжину і ширину прямокутника і обчислює його площу. 
+
+function calcRectangle () {
+    let sideA = prompt (`enter side a`);
+    let sideB = prompt (`enter side b`);
+    let Rectangle = sideA * sideB;
+   return Rectangle;
+}
+ alert (calcRectangle ());
+;
+
+Створи функцію, яка буде виводити кількість переданих їй аргументів??????
+
+function calcArgument () {
+ return `in function ${arguments.length} arguments `;
+};
+calcArgument ();
+
+Напиши функцію, яка приймає 2 числа і повертає :
+-1, якщо перше число менше, ніж друге; 
+1 - якщо перше число більше, ніж друге; 
+0 - якщо числа рівні.
+
+ const function = (firstNumber, secondNumber) => {
+       if (firstNumber < secondNumber) {
+        return -1;
+     } 
+     else if (firstNumber > secondNumber) {
+       return 1;
+       }
+      return 0;
+     };
+     
+ Напиши функцію, яка приймає три окремі цифри і перетворює їх в одне число. Наприклад: цифри 1, 4, 9 перетворяться в число 149.
+
+     const Number = [1, 4, 9];
+     function string () {
+        console.log (Number.join(''));
+     }
+     string ();*/
